8cbbc33eb16d50708b252820ad02038c59688edf
Shuddho Sharaf <79573656+shuddho-sharaf@users.noreply.github.com>
2024-01-08 04:49:29 +0600

    Update ttable.js to v2
    - Add DOM support
    - Add JSON Output

--- ttable.js
+++ ttable.js
@@ -1,101 +1,90 @@
-function TruthTable(str) {
-  const expr = str
+function preprocess(expression) {
+  const cleanExpression = expression
     .toLowerCase()
     .trim()
     .replaceAll(/(xor)/g, "^")
-    .replaceAll(/(not)|~/g, "!")
+    .replaceAll(/(not)|[!]/g, "~")
     .replaceAll(/(and)|[.]/g, "&")
     .replaceAll(/(or)|[+]/g, "|");
 
-  function getVars(logic) {
-    const letters = "abcdefghijklmnopqrstuvwxyz";
-    const vars = [];
+  return cleanExpression;
+}
 
-    let last = "";
+function evaulate(expression) {
+  const letters = "abcdefghijklmnopqrstuvwxyz";
 
-    for (let i = 0; i < letters.length; i++) {
-      for (let j = 0; j < logic.length; j++) {
-        if (logic[j] === letters[i]) {
-          if (logic[j] === last) continue;
-          vars.push(logic[j]);
-          last = logic[j];
-        }
+  const variables = [];
+
+  let last = "";
+
+  for (let i = 0; i < letters.length; i++) {
+    for (let j = 0; j < expression.length; j++) {
+      if (expression[j] === letters[i]) {
+        if (expression[j] === last) continue;
+        variables.push(expression[j]);
+        last = expression[j];
       }
     }
-
-    return vars;
   }
 
-  let vars = getVars(expr);
-
-  if (vars.length < 0 || vars.length > 4) {
-    throw new Error(
-      `The expression ''${expr}' containes either no variable or more than 4 variables.`,
-      { cause: { code: "LimitError" } },
-    );
-  }
-
-  if (vars.length === 1 && expr.search(/[\^\&\|]+/g) !== -1) {
-    throw new Error(
-      `The expression '${expr}' contains only one variable but one or more binary operators.`,
-      { cause: { code: "OperatorError" } },
-    );
-  }
-
-  const logic = Function(vars, `return ${expr}`);
-
-  function inputsFor(rows, columns) {
-    const inputLines = [];
-
-    for (let i = 0; i < rows; i++) {
-      let bits = i.toString(2);
-
-      if (bits.length < columns) {
-        bits = [...String(bits).padStart(columns, "0")];
-      }
-      const numBits = [];
-      for (let i = 0; i < bits.length; i++) {
-        numBits[i] = Number(bits[i]);
-      }
-
-      inputLines.push(numBits);
-    }
-
-    return inputLines;
-  }
-
-  const columns = vars.length;
+  const columns = variables.length;
   const rows = 2 ** columns;
 
-  const cases = inputsFor(rows, columns);
+  const inputs = [];
 
-  function Tabulize(cases, expr) {
-    const states = {};
-    for (let i = 0; i < rows; i++) {
-      states[`state_${i + 1}`] = {
-        logic: expr,
-        inputs: {},
-      };
+  for (let i = 0; i < rows; i++) {
+    let bits = i.toString(2);
 
-      for (let j = 0; j < columns; j++) {
-        states[`state_${i + 1}`]["inputs"][String.fromCharCode(97 + j)] =
-          cases[i][j];
-      }
-
-      states[`state_${i + 1}`]["output"] = Number(`${logic(...cases[i])}`);
+    if (bits.length < columns) {
+      bits = [...String(bits).padStart(columns, "0")];
     }
 
-    return states;
+    const numBits = [];
+
+    for (let i = 0; i < bits.length; i++) {
+      numBits[i] = Number(bits[i]);
+    }
+
+    inputs.push(numBits);
   }
 
-  return Tabulize(cases, expr);
+  const operator = Function(variables, `return ${expression}`);
+
+  const states = {
+    boolean_logic: expression,
+    variables: columns,
+    states: rows,
+  };
+
+  for (let i = 0; i < rows; i++) {
+    states[`state_${i + 1}`] = {
+      inputs: {},
+    };
+
+    for (let j = 0; j < columns; j++) {
+      states[`state_${i + 1}`]["inputs"][String.fromCharCode(97 + j)] =
+        inputs[i][j];
+    }
+
+    states[`state_${i + 1}`]["output"] = Number(`${operator(...inputs[i])}`);
+  }
+
+  return states;
 }
 
-// This section is for testing.
-// Recommended Runtime: Node.js
+const input = document.getElementById("input");
 
-try {
-  console.log(TruthTable("a xor b and c or d"));
-} catch (err) {
-  console.log(`${err.cause.code}: ${err.message}`);
-}
+const button = document.getElementById("submit");
+
+button.addEventListener("click", () => {
+  let inp = input.textContent;
+  const obj = evaulate(preprocess(String(inp)));
+  const out = JSON.stringify(obj, null, 4);
+
+  document.getElementById(
+    "logic"
+  ).innerHTML = `<h2>LOGIC: <code style="color:red;">${obj.boolean_logic}</code></h2>`;
+  document.getElementById("table").innerHTML = `<h2>TABLE:</h2>`;
+
+  document.getElementById("board").innerText = `${out}`;
+});
